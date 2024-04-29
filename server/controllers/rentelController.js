12b9ff0720c42d6449a3cefac20f26d09c225b0d
Aromal M <aromalm011@gmail.com>
2024-04-29 21:42:40 +0530

    controllers rentel, user, vehicle

---
+++ server/controllers/rentelController.js
@@ -0,0 +1,17 @@
+async function bookings_create(req, res) {
+
+}
+
+async function bookings_list_by_user(req, res) {
+
+}
+
+async function bookings_list_admin(req, res) {
+
+}
+
+async function bookings_list_by_renter(req, res) {
+
+}
+
+module.exports = { bookings_create, bookings_list_by_user, bookings_list_admin, bookings_list_by_renter }
